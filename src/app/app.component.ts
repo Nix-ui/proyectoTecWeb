ef32227cc17fa7a053db3b64302c3cbfeb343908
Jesus Ariel Mamani Gutierrez <ariel.10.mg2000@gmail.com>
2025-06-03 23:11:54 +0200

    Implementa el nuevo estilo para registrar, pagina de todos los usuarios y detalles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component , OnInit} from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
 import { ApiClientService } from './servicios/api-client.service';
 import { environment } from '../environments/environment';
 import { HttpHeaders } from '@angular/common/http';
 import { UsuariosComponent } from "./paginas/usuarios/usuarios.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UsuariosComponent, RouterLink],
+  imports: [RouterOutlet, UsuariosComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
